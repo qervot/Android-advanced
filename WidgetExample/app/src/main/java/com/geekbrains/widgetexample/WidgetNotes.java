95f57785bf7aa4df69e3b0c50285dde7f1c8cf69
qervot <10sis@mail.ru>
2017-12-25 18:39:24 +0500

    Widget app

---
+++ WidgetExample/app/src/main/java/com/geekbrains/widgetexample/WidgetNotes.java
@@ -0,0 +1,6 @@
+package com.geekbrains.widgetexample;
+
+import android.appwidget.AppWidgetProvider;
+
+public class WidgetNotes extends AppWidgetProvider {
+}
---
+++ WidgetExample2/app/src/main/java/com/geekbrains/widgetexample2/NotesWidgetService.java
@@ -0,0 +1,10 @@
+package com.geekbrains.widgetexample2;
+
+import android.content.Intent;
+import android.widget.RemoteViewsService;
+
+public class NotesWidgetService extends RemoteViewsService {
+    public RemoteViewsService.RemoteViewsFactory onGetViewFactory(Intent intent) {
+        return new WidgetNotesFactory(this.getApplicationContext(), intent);
+    }
+}
